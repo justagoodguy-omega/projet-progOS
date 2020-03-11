8b058750f04f6d04d00f6644069b321b10a37ab0
Lorenzo Carlo Rovati <lorenzo.rovati@epfl.ch>
2020-03-11 09:22:28 +0100

    added bit.c

---
+++ bit.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdint.h>   // for uint8_t and uint16_t types
+#include <inttypes.h> // for PRIx8, etc.
+
+// ======================================================================
+/**
+ * @brief a type to represent 1 single bit.
+ */
+/* Nous vous fournission ici un type à n'utiliser QUE lorsque vous
+ * voulez représenter UN SEUL bit ; p.ex. :
+ *     bit_t un_bit_tout_seul = 1;
+ */
+typedef uint8_t bit_t;
+
+
+// ======================================================================
+/**
+ * @brief clamp a value to be a bit index between 0 and 7
+ */
+/* Nous vous fournission ici une macro (les macros seront présentées dans
+ *     le cours, bien plus tard dans le semestre) permettant de forcer
+ * une valeur entre 0 et 7.
+ * Par exemple :
+ *     i = CLAMP07(j);
+ * fera que la variable i contiendra :
+ *     + la même valeur que celle de j si celle-ci est comprise entre 0 et 7 ;
+ *     + 0 si la valeur de j est inférieure ou égale à 0 ;
+ *     + 0 si la vlaeur de j est supérieure ou égale à 8.
+ */
+#define CLAMP07(x) (((x) < 0) || ((x) > 7) ? 0 : (x))
+
+
+// ======================================================================
+#define size(T) (sizeof T / sizeof T[0])
+#define FMT8 "0x%02" PRIx8
+#define FMT16 "0x%04" PRIx16
+#define show(M, S1) printf(#M "(" FMT ## S1 ") = " FMT8 "\n", value, result)
+#define show2(M, V) printf("bit_" #M "(" FMT8 ", %d) = " FMT8 "\n", V, k, value2)
+
+
+// ======================================================================
+/**
+ * @brief returns the 4 LSB of a uint8_t
+ *
+ * @param value value to get the 4 LSB from
+ * @return 4 LSB of the input
+ */
+uint8_t lsb4(uint8_t value){
+    return value & 0xf;
+}
+
+// ======================================================================
+/**
+ * @brief returns the 4 MSB of a uint8_t
+ *
+ * @param value value to get the 4 MSB from
+ * @return 4 MSB of the input
+ */
+uint8_t msb4(uint8_t value){
+    return value >> 4;
+}
+
+// ======================================================================
+/**
+ * @brief returns the 8 LSB of a uint16_t
+ *
+ * @param value value to get the 8 LSB from
+ * @return 8 LSB of the input
+ */
+uint8_t lsb8(uint16_t value){
+    return value & 0xff;
+}
+
+// ======================================================================
+/**
+ * @brief returns the 8 MSB of a bit16_t (uint16_t)
+ *
+ * @param value value to get the 8 MSB from
+ * @return 8 MSB of the input
+ */
+uint8_t msb8(uint16_t value){
+    return value >> 8;
+}
+
+// ======================================================================
+/**
+ * @brief Merges two 8bit into 16bits
+ *
+ * @param v1 value for the 8 LSB
+ * @param v2 value for the 8 MSB
+ * @return result of merging v1 and v2 into 16bits
+ */
+uint16_t merge8(uint8_t v1, uint8_t v2){
+    return ((v2 & 0xff) << 8) | (v1 & 0xff);
+}
+
+// ======================================================================
+/**
+ * @brief Merges two 4bit into 8bits
+ *
+ * @param v1 value for the 4 LSB
+ * @param v2 value for the 4 MSB
+ * @return result of merging v1 and v2 into 8bits
+ */
+uint8_t merge4(uint8_t v1, uint8_t v2){
+    return ((v2 & 0xf) << 4) | (v1 & 0xf);
+}
+
+// ======================================================================
+/**
+ * @brief Gets the bit at a given index
+ *
+ * @param value value to get the bit from
+ * @param index index of the bit
+ * @return returns the bit at a given index
+ */
+bit_t bit_get(uint8_t value, int index){
+    return ((value & (1 << CLAMP07(index))) == 0) ? 0 : 1;
+}
+
+// ======================================================================
+/**
+ * @brief Set the bit at a given index to 1
+ *
+ * @param value value to change the bit in
+ * @param index index of the bit
+ */
+void bit_set(uint8_t* value, int index){
+    *value =  *value | (1 << CLAMP07(index));
+}
+
+// ======================================================================
+/**
+ * @brief Set the bit at a given index to 0
+ *
+ * @param value value to change the bit in
+ * @param index index of the bit
+ */
+void bit_unset(uint8_t* value, int index){
+    *value = *value & ~(1 << CLAMP07(index));
+}
