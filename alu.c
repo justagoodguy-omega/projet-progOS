7dd7acf7df4dbae6b9fb37a75a0452bf31b9aa5d
Paul Oliver <paul.oliver@epfl.ch>
2020-03-18 15:35:44 +0100

    my part of alu.c added

--- alu.c
+++ alu.c
@@ -25,6 +25,7 @@ void set_flag(flags_t* flags, flag_bit_t flag){
     *flags = *flags | flag;
 }
 
+//=====================================================================================================================
 /**
  * @brief adds two uint8 and writes the results and flags into an alu_output_t structure
  *
@@ -56,6 +57,7 @@ int alu_add8(alu_output_t* result, uint8_t x, uint8_t y, bit_t c0){
     return ERR_NONE;
 }
 
+//==============================================================================================================
 /**
  * @brief subtract two uint8 and writes the results and flags into an alu_output_t structure
  *
@@ -90,6 +92,7 @@ int alu_sub8(alu_output_t* result, uint8_t x, uint8_t y, bit_t b0){
     return ERR_NONE;
 }
 
+//===========================================================================================================
 /**
  * @brief sum two uint16 and writes the results and flags into an alu_output_t structure,
  *        the H & C flags are being placed according to the 8 lsb
@@ -123,6 +126,7 @@ int alu_add16_low(alu_output_t* result, uint16_t x, uint16_t y){
     return ERR_NONE;
 }
 
+//===========================================================================================================
 /**
  * @brief sum two uint16 and writes the results and flags into an alu_output_t structure,
  *        the H & C flags are being placed according to the 8 msb
@@ -155,3 +159,141 @@ int alu_add16_high(alu_output_t* result, uint16_t x, uint16_t y){
 
     return ERR_NONE;
 }
+
+// ===========================================================================
+/**
+ * @brief logical shift by 1 in direction of dir. The fanions are Z00C
+ * 
+ * @param result The value we will store the result and flags of the operation 
+ * @param x the uint that will be shifted
+ * @param dir the direction of the shift, LEFT or RIGHT
+ */
+
+int alu_shift(alu_output_t* result, uint8_t x, rot_dir_t dir){
+
+    M_REQUIRE_NON_NULL(result);
+    (result -> flags) = 0;
+
+    switch (dir) {
+	case LEFT:
+		if(bit_get(x, uint8MSBindex) == 1){
+            set_flag(result -> flags, FLAG_C);
+        }
+        if (x >> 1 == 0) {
+            set_flag(result -> flags, FLAG_Z);
+        }
+        result -> result = x << 1;
+        break;
+
+	case RIGHT:
+		if (bit_get(x, 0) == 1){
+            set_flag(result -> flags, FLAG_C);
+        }
+         if (x >> 1 == 0) {
+            set_flag(result -> flags, FLAG_Z);
+        }
+        result -> result = x >> 1;
+        break;
+    }
+    return ERR_NONE;
+}
+
+// ===========================================================================
+/**
+ * @brief arithmetic shift by 1 to the right (incoming bit may not be 0).
+ * The fanions are Z00C. C corresponds to the ejected bit
+ * 
+ * @param result The value we will store the result and flags of the operation 
+ * @param x the uint that will be shifted
+ */
+
+int alu_shiftR_A(alu_output_t* result, uint8_t x){
+
+    M_REQUIRE_NON_NULL(result);
+    (result -> flags) = 0;
+    uint8_t shift_result = x >> 1;
+
+    if (bit_get(x, 0) == 1){
+        set_flag(result -> flags, FLAG_C);
+    }
+    if (bit_get(x, uint8MSBindex) == 1){
+        bit_set(&shift_result, uint8MSBindex);
+    if( shift_result == 0){
+        set_flag(result -> flags, FLAG_Z);
+    }
+    result -> result = shift_result;
+    return ERR_NONE;
+}
+// ======================================================================
+/**
+ * @brief Rotate the bits of x by 1 spot in direction dir and store result 
+ * and flags in result. The fanions are Z00C
+ * 
+ * @param result The value we will store the result and flags of the operation 
+ * @param x uint that will be rotated
+ * @param dir the direction of the rotation, LEFT or RIGHT
+ */
+int alu_rotate(alu_output_t* result, uint8_t x, rot_dir_t dir){
+    M_REQUIRE_NON_NULL(result);
+    (result -> flags) = 0;
+    switch (dir) {
+	case LEFT:
+		if (bit_get(x, uint8MSBindex) == 1) {
+            set_flag(result -> flags, FLAG_C)
+        }
+        break;
+
+	case RIGHT:
+		if (bit_get(x, 0) == 1){
+            set_flag(result -> flags, FLAG_C);
+        }
+        break;
+    }
+    (result -> result) = bit_rotate(*x, dir, 1);
+    if (x == 0 ){
+        set_flag(result -> flags, FLAG_Z);
+    }
+    return ERR_NONE;
+}
+
+// ===========================================================================
+/**
+ * @brief simulate a 9 bit rotation left or right, where the 9th bit (msb) is 
+ * the bit from flag C. The fanions are Z00C
+ * 
+ * @param result The value we will store the result and flags of the operation 
+ * @param x uint that will be rotated
+ * @param dir the direction of the rotation, LEFT or RIGHT
+ * 
+ */
+
+int alu_carry_rotate(alu_output_t* result, uint8_t x, rot_dir_t dir, flags_t flags){
+    M_REQUIRE_NON_NULL(result);
+    (result -> flags) = 0;
+    switch (dir) {
+	case LEFT:
+		bit_t msb_x = bit_get(x, uint8MSBindex);
+        x = x << 1;
+        x = x + flags;
+        if (msb_x == 1){
+            set_flag(result -> flags, FLAG_C);
+        }
+        break;
+
+	case RIGHT:
+		bit_t lsb_x = bit_get(x, 0);
+        x = x >> 1
+        x = x + (flags << uint8MSBindex);
+        if (lsb_x == 1){
+            set_flag(result -> flags, FLAG_C);
+        }
+        break;
+    }
+    if(x == 0){
+        set_flag(result -> flags, FLAG_Z);
+    }
+    result -> result = x;
+    return ERR_NONE; 
+}
+
+
